91873399dcedb98f4865c5ed1242a83c59d70cdc
Petrov Artem <artem_petrov_0407@mail.ru>
2025-11-10 18:37:43 +0300

    fitst commit

---
+++ bot/services/infobaket_api.py
@@ -0,0 +1,18 @@
+import requests
+
+class InfoBasketAPI:
+    BASE_URL = "https://reg.infobasket.su/Widget"
+
+    def __init__(self, team_id: int, comp_id: int):
+        self.team_id = team_id
+        self.comp_id = comp_id
+
+    def get_team_roster(self):
+        url = f"{self.BASE_URL}/TeamRoster/{self.team_id}?compId={self.comp_id}&format=json&lang=ru"
+        r = requests.get(url)
+        return r.json() if r.status_code == 200 else None
+
+    def get_team_schedule(self):
+        url = f"{self.BASE_URL}/TeamSchedule/{self.team_id}?compId={self.comp_id}&format=json&lang=ru"
+        r = requests.get(url)
+        return r.json() if r.status_code == 200 else None
