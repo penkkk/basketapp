b50acf3d6770fbffa042945aeba4e0c78bfd6e54
Petrov Artem <artem_petrov_0407@mail.ru>
2025-12-20 16:20:21 +0300

    add player stat1

--- bot/handlers/inline_player_stat.py
+++ bot/handlers/inline_player_stat.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import CallbackQuery
 from bot.services.get_full_roster import full_roster
 from .get_player_stats import get_player_stat
+from bot.keyboards.inlinekeyboard import inline_roster
 
 router = Router()
 
@@ -11,5 +12,5 @@ async def player_stat_callback(callback: CallbackQuery):
     
     stats = await get_player_stat(person_id=person_id)
     
-    await callback.message.edit_text(f"{stats}")
+    await callback.message.edit_text(f"{stats}", reply_markup= await inline_roster())
     await callback.answer()
