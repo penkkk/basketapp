c6c0b14e1a7a6cd16d09d84e283f8597e7c1ebba
Petrov Artem <artem_petrov_0407@mail.ru>
2025-12-20 14:35:37 +0300

    full roster is func

--- bot/keyboards/inlinekeyboard.py
+++ bot/keyboards/inlinekeyboard.py
@@ -4,7 +4,7 @@ from bot.services.get_full_roster import full_roster
 async def inline_roster():
     keyboard = []
 
-    for name, id in full_roster.items():
+    for name, id in full_roster().items():
         keyboard.append([
             InlineKeyboardButton(
                 text=name,
--- bot/services/get_full_roster.py
+++ bot/services/get_full_roster.py
@@ -2,17 +2,21 @@ from bot.services.infobasket_api import InfoBasketAPI
 from bot.config import Config
 
 
-TEAM_ID = Config.TEAM_ID
-COMP_ID = Config.COMP_ID
-infobasket_api = InfoBasketAPI(TEAM_ID, COMP_ID)
-
-roster = infobasket_api.get_team_roster()
-players = roster.get("Players", [])
-full_roster = {}
-for player in players:
-    person_info = player.get("PersonInfo", {})
-    person_full_name_ru = person_info.get("PersonFullNameRu", "")
-    person_id_roster = person_info.get("PersonID")
-    full_roster[person_full_name_ru] = person_id_roster
+def full_roster():
     
+    TEAM_ID = Config.TEAM_ID
+    COMP_ID = Config.COMP_ID
+
+    infobasket_api = InfoBasketAPI(TEAM_ID, COMP_ID)
+
+    roster = infobasket_api.get_team_roster()
+    players = roster.get("Players", [])
+    full_roster = {}
+    for player in players:
+        person_info = player.get("PersonInfo", {})
+        person_full_name_ru = person_info.get("PersonFullNameRu", "")
+        person_id_roster = person_info.get("PersonID")
+        full_roster[person_full_name_ru] = person_id_roster
+    
+    return full_roster
     
